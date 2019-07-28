e9ef545d453e4e6577fe9517f0267e9a8a6ae913
martaprosniak <martaprosniak@gmail.com>
2019-07-28 23:40:08 +0200

    meters are calculated to inches based on user input

--- src/main/java/com/mp/android/meterstoinches/MainActivity.java
+++ src/main/java/com/mp/android/meterstoinches/MainActivity.java
@@ -3,12 +3,38 @@ package com.mp.android.meterstoinches;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText enteredMeters;
+    private Button convertButton;
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enteredMeters = (EditText) findViewById(R.id.inputInMetersId);
+        resultTextView = (TextView) findViewById(R.id.outputInInchesId);
+        convertButton = (Button) findViewById(R.id.convertButtonId);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double multipler = 39.37;
+                double result = 0.0;
+
+                // gets user input, converts it to String and then parse it to double
+                double metersInput = Double.parseDouble(enteredMeters.getText().toString());
+                result = metersInput * multipler;
+
+                resultTextView.setText(result + " inches");
+            }
+        });
     }
 }
